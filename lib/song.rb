8cea2bcb954363d25f9db01e8126c4da0c007c09
Angela Lo <angelawenlo@gmail.com>
2019-07-02 17:39:12 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+require 'pry'
+class Song
+    extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist) if artist
+    self.genre=(genre) if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+ def artist=(artist)
+   @artist = artist
+   artist.add_song(self)
+ end
+
+ def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+  self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
+
+end
